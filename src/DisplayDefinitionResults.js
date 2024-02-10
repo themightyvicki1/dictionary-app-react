2dae681fd3a78f7307f7131f28ab96a60ddfddfc
themightyvicki1 <victoria.greer861@gmail.com>
2024-02-09 18:07:37 -0800

    some styling, can display two definitions right now need to work on map

--- src/DisplayDefinitionResults.js
+++ src/DisplayDefinitionResults.js
@@ -1,14 +1,22 @@
 import React from "react";
+import "./DisplayDefinitionResults.css";
 
 export default function DisplayDefinitionResults(props) {
   if (props.results) {
     console.log(props.results);
     return (
       <div className="DisplayDefinitionResults">
-        <div className="wordDisplayed">
-          Searching for: <strong>{props.results[0].word}</strong>
-        </div>
-        <div className="phoneticDisplayed">{props.results[0].phonetic}</div>
+        <section>
+          <div className="wordDisplayed">
+            Searching for: <strong>{props.results[0].word}</strong>
+          </div>
+          <div className="phoneticDisplayed">{props.results[0].phonetic}</div>
+        </section>
+        <section>
+          {props.results[0].meanings[0].definitions[0].definition}
+          <br />
+          {props.results[0].meanings[0].definitions[1].definition}
+        </section>
       </div>
     );
   } else {
